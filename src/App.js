64abfe3e519cce2d7533c7bf7e021e72e4119f29
afendi <afendiajip94@gmail.com>
2019-05-14 10:43:14 +0700

    second

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+///compoent
+//state
+//props
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Gua Masih Belajar</p>
+    <ul>
+    <li>Home</li>
+    <li>Data Murid</li>
+    <li>Tambah Murid</li>
+    </ul>
+
     </div>
   );
 }
