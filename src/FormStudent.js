62e4a942abd0c0ee228ab91eef0ac55c5f1ac2eb
afendi <afendiajip94@gmail.com>
2019-05-14 14:29:21 +0700

    component & state input data and show to view

--- src/FormStudent.js
+++ src/FormStudent.js
@@ -1,17 +1,55 @@
-
-//Descturcturing
-
+ // === Descturcturing===
 import React, { Component } from 'react'
+export default class FormStudent extends Component{
 
-export default class FormStudent extends Component(){
+// ====melakukan export dengan cara biasa / umum 	
+// import React from 'react'
+// export default class FormStudent extends React.Component {
+
+
+// ===STATE
 	constructor(){
 		super()
+		this.state={
+			students_array:['Afendi'],
+			currentStudentname :''
+
+		}
 	}
+
+
+addStudent(){
+	let currentStudents = this.state.students_array
+	currentStudents.push(this.state.currentStudentname)
+	this.setState({
+		students_array:currentStudents
+	})
+}
+
 
 	render(){
 		return(
+			
 			<div>
-			THIS IS FORM COMPONENT
+			<span>{this.state.currentStudentname}</span>
+			<br/>
+
+
+			<input type="text" name="student_name" 
+			onChange={(e) => this.setState({currentStudentname : e.target.value})}/>
+
+			<input 
+			type="submit" 
+			value="Simpan"
+			onClick={ () => this.addStudent() }
+			/>
+
+
+
+
+			{this.state.students_array.map( student => {
+				return <h3>{ student }</h3>
+			})}
 			</div>
 
 			)
